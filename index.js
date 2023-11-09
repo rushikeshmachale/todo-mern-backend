b4e926757667dfe84fe15cd8f800a55d4f0974fa
rushikeshmachale <jocky0909@gmail.com>
2023-11-09 18:54:20 +0530

    todo-backend

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import routes from "./routes/routes.js";
 import cors from "cors"
+import dotenv from 'dotenv'
+
 const app = express();
 
+dotenv.config()
 mongoose
-.connect("mongodb://127.0.0.1:27017")
+// .connect("mongodb://127.0.0.1:27017")
+.connect(process.env.MONGO_URI)
 .then(() => console.log("connected to db"))
 .catch(() => console.log("error while connecting to db"));
 app.use(cors())
